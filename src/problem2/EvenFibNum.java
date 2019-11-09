b999f83463ba08057cb0df937062c870cad02e76
JeremyScott17 <s1654170@sms.ed.ac.uk>
2019-11-09 15:46:38 +0000

    Implement isEven method

--- src/problem2/EvenFibNum.java
+++ src/problem2/EvenFibNum.java
@@ -7,10 +7,20 @@ public class EvenFibNum {
 	private static int term1 = 1;
 	private static int term2 = 2;
 	private static int newTerm = 3;
+	
+	private static boolean isEven(int val) {
+		if (val % 2 == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 	public static void main(String[] args) {
 		while (newTerm < limit) {
-			
+			if (isEven(newTerm)) {
+				
+			}
 		}
 	}
 
