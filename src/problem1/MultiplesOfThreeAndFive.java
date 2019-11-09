3f49772170718c2a0f656bfcb02591fedcf08c30
JeremyScott17 <s1654170@sms.ed.ac.uk>
2019-11-09 15:11:05 +0000

    Implement method isMultiple

---
+++ src/problem1/MultiplesOfThreeAndFive.java
@@ -0,0 +1,25 @@
+package problem1;
+
+public class MultiplesOfThreeAndFive {
+	
+	private final static int multiple3 = 3;
+	private final static int multiple5 = 5;
+	private final static int limit = 1000;
+	
+	private static boolean isMultiple(int val) {
+		if (val % multiple3 == 0 || val % multiple5 == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static void main(String[] args) {
+		for (int i = 1; i < limit; i++) {
+			if (isMultiple(i)) {
+				
+			}
+		}
+	}
+
+}
