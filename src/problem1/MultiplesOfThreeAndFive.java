0a40a5d57df208cc5b4658eff88dde17197b2fe6
JeremyScott17 <s1654170@sms.ed.ac.uk>
2019-11-09 21:41:44 +0000

    [Problem 1] + Refactor variable names

--- src/problem1/MultiplesOfThreeAndFive.java
+++ src/problem1/MultiplesOfThreeAndFive.java
@@ -2,13 +2,13 @@ package problem1;
 
 public class MultiplesOfThreeAndFive {
 	
-	private final static int multiple3 = 3;
-	private final static int multiple5 = 5;
-	private final static int limit = 1000;
+	private final static int MULTIPLE3 = 3;
+	private final static int MULTIPLE5 = 5;
+	private final static int LIMIT = 1000;
 	private static int sum = 0;
 	
 	private static boolean isMultiple(int val) {
-		if (val % multiple3 == 0 || val % multiple5 == 0) {
+		if (val % MULTIPLE3 == 0 || val % MULTIPLE5 == 0) {
 			return true;
 		} else {
 			return false;
@@ -16,7 +16,7 @@ public class MultiplesOfThreeAndFive {
 	}
 
 	public static void main(String[] args) {
-		for (int i = 1; i < limit; i++) {
+		for (int i = 1; i < LIMIT; i++) {
 			if (isMultiple(i)) {
 				sum += i;
 			}
