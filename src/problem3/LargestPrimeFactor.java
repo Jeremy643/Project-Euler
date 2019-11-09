db78b06e63a0d2b5ef1e38b21de5ca4973d62273
JeremyScott17 <s1654170@sms.ed.ac.uk>
2019-11-09 22:16:34 +0000

    [Problem 3] + Implement changes to allow numberOfPrimes to vary depending on the   size of number

--- src/problem3/LargestPrimeFactor.java
+++ src/problem3/LargestPrimeFactor.java
@@ -5,13 +5,20 @@ import java.util.List;
 
 public class LargestPrimeFactor {
 	
-	private static int numberOfPrimes = 1;
+	private static int numberOfPrimes = 0;
 	private static long number = 600851475143L;
 	private static List<Integer> primes = new ArrayList<>();
 	private static List<Integer> primeFactors = new ArrayList<>();
 	
 	private static void getPrimes() {
-		int possiblePrime = 2;
+		int possiblePrime;
+		if (primes.size() == 0) {
+			possiblePrime = 2;
+		} else {
+			//carry on from where we ended in previous iteration
+			possiblePrime = primes.get(primes.size()-1) + 1;
+		}
+		
 		do {
 			//counter holds the number of factors possiblePrime has, except for 1 or possiblePrime
 			int counter = 0;
@@ -44,9 +51,14 @@ public class LargestPrimeFactor {
 	}
 
 	public static void main(String[] args) {
-		getPrimes();
+		do {
+			//increase the number of primes we store by 1 until all factors found
+			numberOfPrimes++;
+			getPrimes();
+			getPrimeFactors();
+		} while (number != 1);
 		
-		getPrimeFactors();
+		System.out.println(primeFactors.get(primeFactors.size()-1));
 	}
 
 }
