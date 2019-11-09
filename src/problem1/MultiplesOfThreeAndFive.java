f9225b64f60247b4a8b14252253b4b9675e94278
JeremyScott17 <s1654170@sms.ed.ac.uk>
2019-11-09 15:14:14 +0000

    Implement class MultiplesOfThreeAndFive

--- src/problem1/MultiplesOfThreeAndFive.java
+++ src/problem1/MultiplesOfThreeAndFive.java
@@ -5,6 +5,7 @@ public class MultiplesOfThreeAndFive {
 	private final static int multiple3 = 3;
 	private final static int multiple5 = 5;
 	private final static int limit = 1000;
+	private static int sum = 0;
 	
 	private static boolean isMultiple(int val) {
 		if (val % multiple3 == 0 || val % multiple5 == 0) {
@@ -17,9 +18,11 @@ public class MultiplesOfThreeAndFive {
 	public static void main(String[] args) {
 		for (int i = 1; i < limit; i++) {
 			if (isMultiple(i)) {
-				
+				sum += i;
 			}
 		}
+		
+		System.out.println(sum);
 	}
 
 }
