9d3bce03c9f379417ae6f4811c6ba02754bcc6a9
JeremyScott17 <s1654170@sms.ed.ac.uk>
2019-11-09 15:39:48 +0000

    Implement initial while loop in main

--- src/problem2/EvenFibNum.java
+++ src/problem2/EvenFibNum.java
@@ -1,9 +1,17 @@
 package problem2;
 
 public class EvenFibNum {
+	
+	private static final int limit = 4000000;
+	
+	private static int term1 = 1;
+	private static int term2 = 2;
+	private static int newTerm = 3;
 
 	public static void main(String[] args) {
-		
+		while (newTerm < limit) {
+			
+		}
 	}
 
 }
