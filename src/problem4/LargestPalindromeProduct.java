a762d3d10d0686c4a9b3e8a42e5d8671e9c661dd
JeremyScott17 <s1654170@sms.ed.ac.uk>
2019-11-10 14:09:10 +0000

    Implement getLargestPalindrome method and rough structure of method isPalindrome

--- src/problem4/LargestPalindromeProduct.java
+++ src/problem4/LargestPalindromeProduct.java
@@ -1,9 +1,33 @@
 package problem4;
 
 public class LargestPalindromeProduct {
+	
+	private static int startingNumber = 999;
+	private static int limit = 900;
+	
+	private static boolean isPalindrome(int val) {
+		return true;
+	}
+	
+	private static int getLargestPalindrome() {
+		for (int i = 999; i > 99; i--) {
+			for (int j = startingNumber; j > limit; j--) {
+				int palindrome = i * j;
+				if (isPalindrome(palindrome)) {
+					return palindrome;
+				}
+			}
+			startingNumber -= 1;
+			limit -= 100;
+		}
+		
+		return 0;
+	}
 
 	public static void main(String[] args) {
+		int largestPalindrome = getLargestPalindrome();
 		
+		System.out.println(largestPalindrome);
 	}
 
 }
