53c071c3864d87373d0400af945ecff9f1697b6f
JeremyScott17 <s1654170@sms.ed.ac.uk>
2019-11-12 14:49:36 +0000

    Implement getSquareSums method

--- src/problem6/SumSquareDifference.java
+++ src/problem6/SumSquareDifference.java
@@ -15,7 +15,13 @@ public class SumSquareDifference {
 	}
 	
 	private static int getSquareSums() {
-		return 0;
+		int sum = 0;
+		for (int i = 1; i <= LIMIT; i++) {
+			sum += i;
+		}
+		int square = sum*sum;
+		
+		return square;
 	}
 
 	public static void main(String[] args) {
