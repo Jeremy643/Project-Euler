29107cf868a61972a4715599cd0f0e87c9364ac8
JeremyScott17 <s1654170@sms.ed.ac.uk>
2019-11-12 14:43:14 +0000

    Implement main method and basic structure of helper methods
    Helper methods:
    * getSumSquares
    * getSquareSums

--- src/problem6/SumSquareDifference.java
+++ src/problem6/SumSquareDifference.java
@@ -1,9 +1,23 @@
 package problem6;
 
 public class SumSquareDifference {
+	
+	private static final int LIMIT = 100;
+	
+	private static int getSumSquares() {
+		return 0;
+	}
+	
+	private static int getSquareSums() {
+		return 0;
+	}
 
 	public static void main(String[] args) {
+		int sumSquares = getSumSquares();
+		int squareSums = getSquareSums();
+		int diff = squareSums - sumSquares;
 		
+		System.out.println(diff);
 	}
 
 }
