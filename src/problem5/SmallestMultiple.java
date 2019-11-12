59825d61c2179b560bef07d91294601e020f307d
JeremyScott17 <s1654170@sms.ed.ac.uk>
2019-11-12 00:21:35 +0000

    Implement SmallestMultiple class

--- src/problem5/SmallestMultiple.java
+++ src/problem5/SmallestMultiple.java
@@ -2,8 +2,43 @@ package problem5;
 
 public class SmallestMultiple {
 	
-	public static void main(String[] args) {
+	private static final int LIMIT = 20;
+	private static final int[] DIVISIBLE = new int[LIMIT];
+	
+	private static int answer = 0;
+	
+	private static void initDivisible() {
+		for (int i = 0; i < LIMIT; i++) {
+			DIVISIBLE[i] = i+1;
+		}
+	}
+	
+	private static void getSmallestDivisibleNum() {
+		boolean found = false;
+		int index = 1;
 		
+		do {
+			int counter = 0;
+			for (int div : DIVISIBLE) {
+				if (index % div == 0) {
+					counter++;
+				}
+			}
+			
+			if (counter == 20) {
+				found = true;
+				answer = index;
+			}
+			
+			index++;
+		} while (!found);
+	}
+	
+	public static void main(String[] args) {
+		initDivisible();
+		getSmallestDivisibleNum();
+		
+		System.out.println(answer);
 	}
 
 }
