6f2c7e326156b1ed98772be593b8fc9a612ed26f
JeremyScott17 <s1654170@sms.ed.ac.uk>
2019-11-13 19:58:51 +0000

    Implement LargestProductSeries class

--- src/problem8/LargestProductSeries.java
+++ src/problem8/LargestProductSeries.java
@@ -12,9 +12,10 @@ import java.util.Scanner;
 public class LargestProductSeries {
 	
 	private static final int LIMIT = 1000;
+	private static final int ADJACENT_DIGITS = 13;
 	private static final File FILE = new File("C:\\Users\\jerem\\GitHub\\Project-Euler\\src\\problem8\\1000-digit-number.txt");
 	
-	private static int[] number = new int[LIMIT];
+	private static long[] number = new long[LIMIT];
 	
 	private static void readFile() {
 		String fileContent = "";
@@ -29,12 +30,40 @@ public class LargestProductSeries {
 		}
 		
 		for (int i = 0; i < LIMIT; i++) {
-			number[i] = Integer.parseInt(String.valueOf(fileContent.charAt(i)));
+			number[i] = Long.parseLong(String.valueOf(fileContent.charAt(i)));
 		}
+	}
+	
+	private static long findLargestProduct() {
+		long greatestProduct = 0L;
+		
+		for (int i = 0; i < (LIMIT - ADJACENT_DIGITS); i++) {
+			long currentProduct = number[i];
+			if (number[i] == 0L) {
+				continue;
+			} else {
+				for (int j = i+1; j < (i + ADJACENT_DIGITS); j++) {
+					if (number[j] == 0L) {
+						break;
+					} else {
+						currentProduct *= number[j];
+					}
+				}
+			}
+			
+			if (currentProduct > greatestProduct) {
+				greatestProduct = currentProduct;
+			}
+		}
+		
+		return greatestProduct;
 	}
 
 	public static void main(String[] args) {
 		readFile();
+		long answer = findLargestProduct();
+		
+		System.out.println(answer);
 	}
 
 }
